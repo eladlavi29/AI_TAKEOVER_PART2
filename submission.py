13b3894164ebcd6eb7e18197461768237c9b292e
glazernir <glazer.nir@campus.technion.ac.il>
2024-03-08 13:05:47 +0200

    expectimax

--- submission.py
+++ submission.py
@@ -16,7 +16,6 @@ def smart_heuristic(env: WarehouseEnv, robot_id: int):
     else:
         doesHoldBox = 0
 
-
     if robot.credit > 15:
         h += 10 * robot.credit + doesHoldBox
     else:
@@ -54,10 +53,8 @@ class AgentMinimax(Agent):
     # TODO: section b : 1
     def RBminimax(self, env: WarehouseEnv, agent_id, D, turn=True):
         if env.done() or D == 0:
-            return self.heuristic(env, agent_id), None
-
+            return self.heuristic(env, agent_id), self.successors(env,agent_id)[0][0]
         chosen_step = None
-
         if turn:
             curmax = -np.inf
             operators, children = self.successors(env, agent_id)
@@ -71,14 +68,14 @@ class AgentMinimax(Agent):
             return curmax, chosen_step
 
         curmin = np.inf
-        operators, children = self.successors(env, agent_id)
+        operators, children = self.successors(env,1-agent_id)
         for operator, child in zip(operators, children):
             v = self.RBminimax(child, agent_id, D-1, not turn)[0]
 
             if (v < curmin):
                 curmin = v
                 chosen_step = operator
-        return curmin, chosen_step
+        return curmin, None
 
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
         time_factor = 5
@@ -99,14 +96,106 @@ class AgentMinimax(Agent):
 
 class AgentAlphaBeta(Agent):
     # TODO: section c : 1
-    def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        raise NotImplementedError()
+    def heuristic(self, env: WarehouseEnv, robot_id: int):
+        return smart_heuristic(env, robot_id)
 
+    def ABminimax(self, env: WarehouseEnv, agent_id, D, turn=True,a=-np.inf,b=np.inf):
+        if env.done() or D == 0:
+            return self.heuristic(env, agent_id), self.successors(env,agent_id)[0][0]
+        chosen_step = None
+
+        if turn:
+            curmax = -np.inf
+            operators, children = self.successors(env, agent_id)
+            for operator, child in zip(operators, children):
+                v = self.ABminimax(child, agent_id, D - 1, not turn,a,b)[0]
+                if (v > curmax):
+                    curmax = v
+                    chosen_step = operator
+                a = max(curmax,a)
+                if curmax >= b:
+                    return np.inf,None
+            return curmax, chosen_step
+
+        curmin = np.inf
+        operators, children = self.successors(env, 1-agent_id)
+        for operator, child in zip(operators, children):
+            v = self.ABminimax(child, agent_id, D - 1, not turn,a,b)[0]
+            if (v < curmin):
+                curmin = v
+                chosen_step = operator
+            b = min(curmin, b)
+            if a >= curmin:
+                return -np.inf, None
+        return curmin, None
+
+    def run_step(self, env: WarehouseEnv, agent_id, time_limit):
+        time_factor = 20
+        previous_iteration_duration = 0
+        d = 1
+        step = None
+        while True:
+            if time_limit < time_factor * previous_iteration_duration:
+                return step
+            d += 1
+            start_time = time.time()
+            step = self.ABminimax(env, agent_id, D=d, turn=True)[1]
+            previous_iteration_duration = time.time() - start_time
+            time_limit -= previous_iteration_duration
 
 class AgentExpectimax(Agent):
     # TODO: section d : 1
+    def heuristic(self, env: WarehouseEnv, robot_id: int):
+        return smart_heuristic(env, robot_id)
+    #
+    def probabilities(self,env: WarehouseEnv, agent_id,operator):
+        legal_ops = self.successors(env,agent_id)[0]
+        if operator not in legal_ops:
+            return 0.0
+        is_move_right = ('move east' in legal_ops)
+        is_collect_package = ('pick up' in legal_ops)
+        sum = len(legal_ops) + is_move_right + is_collect_package
+        if operator == 'move east' or operator == 'pick up':
+            return 2/sum
+        else:
+            return 1/sum
+
+    def expectimax(self, env: WarehouseEnv, agent_id, D, turn=True):
+        if env.done() or D == 0:
+            return self.heuristic(env, agent_id), self.successors(env,agent_id)[0][0]
+        chosen_step = None
+        if turn:
+            curmax = -np.inf
+            operators, children = self.successors(env, agent_id)
+            for operator, child in zip(operators, children):
+                v = self.expectimax(child, agent_id, D-1, not turn)[0]
+                if(v > curmax):
+                    curmax = v
+                    chosen_step = operator
+            return curmax, chosen_step
+        expectation = 0
+        operators, children = self.successors(env, 1-agent_id)
+        for operator, child in zip(operators, children):
+            v = self.expectimax(child, agent_id, D-1, not turn)[0]
+            expectation += self.probabilities(env,agent_id,operator)*v
+        return expectation, None
+
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        raise NotImplementedError()
+
+        time_factor = 10
+        previous_iteration_duration = 0
+        d = 1
+        step = None
+        while True:
+            if time_limit < time_factor * previous_iteration_duration:
+                print(d)
+                return step
+            d += 1
+            start_time = time.time()
+            step = self.expectimax(env, agent_id, D=d, turn=True)[1]
+            previous_iteration_duration = time.time() - start_time
+            time_limit -= previous_iteration_duration
+
 
 
 # here you can check specific paths to get to know the environment
